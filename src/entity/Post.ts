2ddf3f13f95c2037fc82059fb0750b0af04f41da
Kevin Nguyen <me@kndwin.dev>
2021-04-01 23:49:37 +1100

    ➕ added related to Post and User entities

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,19 +1,27 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {
+  Entity, PrimaryGeneratedColumn, Column,
+  ManyToOne
+} from "typeorm";
+
+import { User } from './User'
 
 @Entity()
 export class Post {
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({
-      length: 100
-    })
-    title: string;
+  @Column({
+    length: 100
+  })
+  title: string;
 
-    @Column({
-      type: "text"  
-    })
-    markdown: string;
+  @Column({
+    type: "text"  
+  })
+  markdown: string;
+
+  @ManyToOne(type => User, user => user.posts)
+  user: User;
 
 }
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,15 +1,23 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {
+  Entity, PrimaryGeneratedColumn, Column,
+  OneToMany
+} from "typeorm";
+
+import { Post } from './Post'
 
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column()
-    source: string;
+  @Column()
+  source: string;
+
+  @OneToMany(type => Post, post => post.user)
+  posts: Post[]; 
 
 }
