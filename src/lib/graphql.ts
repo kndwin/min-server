8cc55d8a5f22c1aa2f5e9335c0d881a034b3c8c9
Kevin Nguyen <me@kndwin.dev>
2021-04-18 11:28:54 +1000

    ➕ feat: added mutation createUser

--- src/lib/graphql.ts
+++ src/lib/graphql.ts
@@ -13,12 +13,15 @@ export var schema = buildSchema(`
 	type Query {
 		getUser(name: String!): User
 	}												 
+
+	type Mutation {
+		createUser(name: String!, source: String!): User
+	}
 `)
 
 export var root = {
 	// check if user exist
 	getUser: ({ name }) => {
-
 		let ans = db().then(async connection => {
 			let userRepo = connection.getRepository( Users )
 			let userFound = await userRepo.findOne({ name: name })
@@ -34,14 +37,47 @@ export var root = {
 			} else {
 				console.log(`User ${userFound} doesn't exist`)
 				connection.close()
-				return false
+				const nullUserJson = {
+					name: null,
+					source: null,
+				}
+				return nullUserJson
 			}
 		}).catch(error => {
 			console.log(error)
 			return error
 		})
 		return ans
-
 	},
-	
+	createUser: ({ name, source }) => {
+		let ans = db().then(async connection => {
+
+			const userRepo = connection.getRepository( Users )
+			const userFound = await userRepo.findOne({ name: name })
+
+			if (userFound !== undefined) {
+				console.log(`User ${userFound.name} exist`)
+				connection.close()
+				const nullUserJson = {
+					name: null,
+					source: null,
+				}
+				return nullUserJson
+
+			} else {
+				console.log(`User ${userFound} doesn't exist`)
+				const newUser = {
+					name: name,
+					source: source,
+				}
+				await userRepo.save(newUser)
+				connection.close()
+				return newUser
+			}
+		}).catch(error => {
+			console.error(error)
+			return error
+		})
+		return ans
+	}
 }
